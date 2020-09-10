6474b7a0a252a87ecd1082b4e858448ac40a31f1
DucTerry <dev.ndangduc.bn@gmail.com>
2020-09-10 14:07:12 +0700

    DucND: Add Methoad GetAll User

--- USER-MANAGEMENT/src/main/java/com/ndangduc/bn/usermanagement/service/impl/UserServiceImpl.java
+++ USER-MANAGEMENT/src/main/java/com/ndangduc/bn/usermanagement/service/impl/UserServiceImpl.java
@@ -13,6 +13,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class UserServiceImpl implements IUserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
@@ -22,11 +25,11 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public UserDTO createUser(CreateUserRequest createUserRequest) {
-        LOGGER.info("[Create User]   --- :  Call Service createUser" );
+        LOGGER.info("[Create User]   --- :  Call Service createUser");
 
         // Kiểm tra xem user đã tồn tại chưa. Nếu có bắn ra Exception
         boolean isEmailExist = userRepository.existsByEmail(createUserRequest.getEmail());
-        LOGGER.info("[Create User]   --- :  Check Exists: " + isEmailExist );
+        LOGGER.info("[Create User]   --- :  Check Exists: " + isEmailExist);
         if (isEmailExist) {
             throw new DuplicatedException("User existed in My System");
         }
@@ -38,7 +41,19 @@ public class UserServiceImpl implements IUserService {
         userRepository.save(user);
 
         // Trả ra kết quả cho Client
-        LOGGER.info("[Create User]   --- : Result Call: " + user );
+        LOGGER.info("[Create User]   --- : Result Call\n" + user);
         return UserMapper.convertToUserDTO(user);
+    }
+
+    @Override
+    public List<UserDTO> getAllUser() {
+        LOGGER.info("[Get All User]   --- :  Call Service Get All User");
+        List<User> userList = userRepository.findAll();
+        List<UserDTO> userDTOS = new ArrayList<>();
+        userList.forEach(u -> {
+            userDTOS.add(UserMapper.convertToUserDTO(u));
+        });
+        LOGGER.info("[Get All User]   --- :  Result Call\n" + userDTOS);
+        return userDTOS;
     }
 }
