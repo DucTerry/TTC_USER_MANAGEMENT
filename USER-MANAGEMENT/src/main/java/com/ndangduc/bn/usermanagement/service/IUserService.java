90d79cf6b716bfd6de593ec4672e34df731324b4
DucTerry <dev.ndangduc.bn@gmail.com>
2020-09-10 14:06:57 +0700

    DucND: Add Methoad GetAll User

--- USER-MANAGEMENT/src/main/java/com/ndangduc/bn/usermanagement/service/IUserService.java
+++ USER-MANAGEMENT/src/main/java/com/ndangduc/bn/usermanagement/service/IUserService.java
@@ -4,7 +4,10 @@ import com.ndangduc.bn.usermanagement.model.request.CreateUserRequest;
 import com.ndangduc.bn.usermanagement.model.response.UserDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface IUserService {
     UserDTO createUser(CreateUserRequest createUserRequest);
+    List<UserDTO> getAllUser();
 }
